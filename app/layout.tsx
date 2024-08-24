7de2b26200d25e006474b19975941a0236a36e0a
Misbahur Rahman <misbahurbd@gmail.com>
2024-08-24 13:43:37 +0600

    update google tag manager code

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/header"
 import ContacFormModalProvider from "@/providers/contact-form-modal-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import Footer from "@/components/home/footer"
+import { GoogleTagManager } from "@next/third-parties/google"
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -29,21 +30,8 @@ export default function RootLayout({
       suppressHydrationWarning
       lang="en"
     >
-      <head>
-        <script>
-          {(function (w, d, s, l, i) {
-            w[l] = w[l] || []
-            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-            var f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != "dataLayer" ? "&l=" + l : ""
-            j.async = true
-            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-            f.parentNode.insertBefore(j, f)
-          })(window, document, "script", "dataLayer", "GTM-PKMT3WZL")}
-        </script>
-      </head>
       <body>
+        <GoogleTagManager gtmId="GTM-PKMT3WZL" />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
