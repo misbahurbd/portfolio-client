0ec422467c670a4c858f636c42bc96b302cacd88
Misbahur Rahman <misbahurbd@gmail.com>
2024-08-24 13:39:49 +0600

    update google tag manager code

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,20 @@ export default function RootLayout({
       suppressHydrationWarning
       lang="en"
     >
+      <head>
+        <script>
+          {(function (w, d, s, l, i) {
+            w[l] = w[l] || []
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : ""
+            j.async = true
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+            f.parentNode.insertBefore(j, f)
+          })(window, document, "script", "dataLayer", "GTM-PKMT3WZL")}
+        </script>
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
